def8356ef6ccf8d9d36a94a4ec55ff871d879547
jvfe <jvfecav@gmail.com>
2020-08-20 17:10:47 -0300

    Atualiza lista e refatora declaração de variáveis
    Declaração agora segue o padrão de JS. Também altera a frase mostrada,
    para corresponder ao nome da extensão.

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ let element = document.getElementsByClassName("article")[1];
 let text = element.textContent;
 
 // Lista de migués - Colabore aqui!
-migues = ["upon request", "on request", "data not available", "supplementary material"]
+let migues = ["upon request", "on request", "data not available", "supplementary material", "reasonable request"]
 
-result = migues.find(migue => text.includes(migue))
+let result = migues.find(migue => text.includes(migue))
 
 // Checagem
 if (result) {
 
-    title = document.getElementsByClassName("content-title")[0]
+    let title = document.getElementsByClassName("content-title")[0]
 
     // Criação e ligação do elemento HTML
 
-    aviso = document.createElement('div');
-    aviso_text = document.createTextNode(`Miguezômetro achou esse artigo suspeito... Ele contém as palavras-chave "${result}"`);
+    let aviso = document.createElement('div');
+    aviso_text = document.createTextNode(`Migué-detector achou esse artigo suspeito... Ele contém as palavras-chave "${result}"`);
     aviso.appendChild(aviso_text);
     aviso.setAttribute("style", "border: 1px solid darkviolet;background-color: thistle;padding: 0.3923em 0.6923em;border-radius: 5px;");
     title.parentElement.appendChild(aviso);
